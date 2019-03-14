4bccbfc46807917b16b39a72867980d16f06e4ba
Dereck Tu <dereck.tu@hotmail.com>
2019-03-14 17:48:42 -0400

    Implement peek functions

--- Linked List/linked_list.c
+++ Linked List/linked_list.c
@@ -1,1 +1,6 @@
-#include "linked_list.h"
+#include "linked_list.h"node peek_front(linkedlist l){
+	return l->start; //Start of linked list
+}
+node peek_back(linkedlist l){
+	return l->end; //End of linked list
+}
