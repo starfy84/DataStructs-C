997b723e3ce0ee810d034315f480820b2f7a292e
Dereck Tu <dereck.tu@hotmail.com>
2019-03-14 17:50:03 -0400

    Implement struct initialization

--- Linked List/linked_list.c
+++ Linked List/linked_list.c
@@ -5,3 +5,12 @@ node peek_front(linkedlist l){
 node peek_back(linkedlist l){
 	return l->end; //End of linked list
 }
+node make_node(int value){
+	node n = (node)malloc(sizeof(struct NODE)); //Allocate memory
+	n->value = value;
+	n->error_code = 0;
+	return n;
+}
+linkedlist make_linkedlist(){
+	linkedlist l = (linkedlist)malloc(sizeof(struct LINKEDLIST)); //Allocate memory
+	return l;
